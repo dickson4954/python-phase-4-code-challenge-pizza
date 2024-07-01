2e496de082e5de4d38f35357da0337e3c5b7567c
dickson4954 <dicksonmurithi155@gmail.com>
2024-07-01 09:23:38 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, jsonify, make_response,request
 from flask_restful import Api, Resource
 import os
 
@@ -13,7 +13,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db,render_as_batch=True)
 
 db.init_app(app)
 
@@ -24,6 +24,103 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route('/restaurants', methods=['GET'])
+def restaurants():
+    restaurants = []
+    for restaurant in Restaurant.query.all():
+        restaurant_dict = {
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address
+        }
+        restaurants.append(restaurant_dict)
+    return jsonify(restaurants), 200
 
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    
+    if restaurant:
+        restaurant_dict = {
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address,
+            'restaurant_pizzas': [
+                {
+                    'id': restaurant_pizza.id,
+                    'restaurant_id': restaurant_pizza.restaurant_id,
+                    'pizza_id': restaurant_pizza.pizza_id,
+                    'price': restaurant_pizza.price,
+                    'pizza': {
+                        'id': restaurant_pizza.pizza.id,
+                        'name': restaurant_pizza.pizza.name,
+                        'ingredients': restaurant_pizza.pizza.ingredients
+                    }
+                } for restaurant_pizza in restaurant.restaurant_pizzas
+            ]
+        }
+        response =make_response(
+            jsonify(restaurant_dict),
+         200
+         )
+        return response
+    else:
+        response=make_response(
+            jsonify({'error': 'Restaurant not found'}),
+            404
+        )
+        return response
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    
+    pizza_list = []
+    for pizza in pizzas:
+        pizza_dict = {
+            "id": pizza.id,
+            "ingredients": pizza.ingredients,
+            "name": pizza.name
+        }
+        pizza_list.append(pizza_dict)
+    
+    return jsonify(pizza_list)        
+@app.route('/restaurant_pizzas',methods=['POST'])
+def add_pizza_post():
+    if request.method == 'POST':
+       try:
+            restaurant_pizza= RestaurantPizza(
+                 price = request.get_json()["price"],
+                 restaurant_id= request.get_json()["restaurant_id"],
+                 pizza_id= request.get_json()["pizza_id"]
+            )
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+            response= make_response(restaurant_pizza.to_dict(),201,{"content-type":"application/json"})
+            return response
+       except ValueError as e:
+        message = {"errors": ["validation errors"]}
+        response = make_response(message, 400)
+        return response
+            
+@app.route('/restaurants/<int:id>',methods=['DELETE'])
+def del_restaurant(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+    if restaurant:
+       RestaurantPizza.query.filter_by(restaurant_id=id).delete()
+       db.session.delete(restaurant)
+       db.session.commit()
+       response= make_response(
+             jsonify({'message': 'Restaurant is deleted successfully'}),
+             204
+            )
+       return response
+    else:
+        response= make_response(
+             jsonify({'message': 'Restaurant is not found'}),
+             404
+            )
+        return response
+       
+            
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -20,9 +20,14 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
+    restaurant_pizzas = db.relationship('RestaurantPizza',back_populates='restaurant',cascade='all,delete-orphan')
+
+
+
     # add relationship
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -34,26 +39,41 @@ class Pizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza',cascade='all,delete-orphan')
+    
+
 
     # add relationship
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
 
 
 class RestaurantPizza(db.Model, SerializerMixin):
-    __tablename__ = "restaurant_pizzas"
+    __tablename__= "restaurant_pizzas"
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza = db.relationship("Pizza", back_populates='restaurant_pizzas')
+    restaurant = db.relationship("Restaurant", back_populates='restaurant_pizzas')
+    
     # add relationships
 
     # add serialization rules
+    serialize_rules = ('-restaurant.restaurants_pizzas', '-pizza.restaurant_pizzas',)
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be between 1 and 30")
+        return value
+        
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
